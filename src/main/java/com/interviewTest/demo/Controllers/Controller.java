c6daabd5dbfe39d5107b90665d05b926b61254da
Karl bárðarson <kalligust3@gmail.com>
2023-04-06 19:08:42 +0000

    Code cleaned up. Dev helper functions removed

--- src/main/java/com/interviewTest/demo/Controllers/Controller.java
+++ src/main/java/com/interviewTest/demo/Controllers/Controller.java
@@ -1,11 +1,8 @@
 package com.interviewTest.demo.Controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +13,6 @@ import com.interviewTest.demo.Http.RequestObject;
 import com.interviewTest.demo.Http.ResponseObject;
 import com.interviewTest.demo.Models.Position;
 import com.interviewTest.demo.Models.Vessel;
-import com.interviewTest.demo.Repositories.VesselRepository;
 import com.interviewTest.demo.Services.VesselService;
 
 @RestController
@@ -24,17 +20,7 @@ import com.interviewTest.demo.Services.VesselService;
 public class Controller {
 
   @Autowired
-  private VesselRepository vesselRepo;
-
-  @Autowired
   private VesselService vesselService;
-
-  @PostMapping(value = "/addVessel", consumes = "application/json", produces = "application/json")
-  public Vessel addVessel(@RequestBody Vessel vessel) {
-    Vessel newVessel = vessel;
-    Vessel v = vesselService.save(newVessel);
-    return v;
-  }
 
   @PostMapping(value = "/req", consumes = "application/json", produces = "application/json")
   public ResponseEntity<ResponseObject> addPosition(@RequestBody RequestObject req) {
@@ -49,8 +35,4 @@ public class Controller {
 
   }
 
-  @GetMapping("/vessels")
-  public List<Vessel> getVessels() {
-    return vesselRepo.findAll();
-  }
 }
